1a4eb22275be93b8d1cede5fb5f7e6e42b8f11cb
Fernanda <a01781983@tec.mx>
2023-11-15 21:24:46 -0600

    commentarios pertinentes al codigo

--- Unity Files/Cubito/Assets/Scripts/Apply_transforms.cs
+++ Unity Files/Cubito/Assets/Scripts/Apply_transforms.cs
@@ -1,27 +1,32 @@
+//Sylvia Fernanda Colomo Fuente A01781983
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Apply_transforms : MonoBehaviour
 {
-    //para el carro
+    //para el carro se declara el vector de desplazamiento, el angulo y el eje de rotacion
     [SerializeField] Vector3 displacement;
     [SerializeField] float angle;
+    //este ángulo será calculado más adelante para evitar tener que calcularlo manualmente 
     [SerializeField] AXIS rotationAXIS;
 
     //Ruedas, se van a instanciar, se usa un gameobject a instanciar
+    //en el inspector se arrastrará el modelo de la rueda a usar 
     [SerializeField] GameObject wheelModel;
 
-    Mesh mesh;
+    Mesh mesh;//mesh del carro
     Vector3[] vertices;
     Vector3[] newVertices;
     //uso de vertices y newvertices para las transformaciones
 
-    [SerializeField] Wheel[] wheel;//Lista de ruedas 
+    [SerializeField] Wheel[] wheel;//Lista de ruedas (clase ya definida)
 
     // Start is called before the first frame update
     void Start()
     {
+        //Se obtiene el mesh del carro y en base a eso se obtienen los vertices
         mesh = GetComponentInChildren<MeshFilter>().mesh;
         vertices = mesh.vertices;
 
@@ -30,6 +35,8 @@ public class Apply_transforms : MonoBehaviour
         System.Array.Copy(vertices, newVertices, vertices.Length);
 
         //Intancia las ruedas 
+        //por cada elemento de tipo Wheel en la lista se instancia un gameobject con sus debidos atributos 
+        //al igual que el carro se usa una mesh, vertices y newvertices
         for(int i = 0; i<wheel.Length; i++){
             GameObject temp = Instantiate(wheelModel, new Vector3(0,0,0), Quaternion.identity);
 
@@ -47,6 +54,7 @@ public class Apply_transforms : MonoBehaviour
     void Update()
     {
         DoTransform();
+        //se coloca el método en Update ya que se quiere que se actualice cada frame
     }
 
     void DoTransform(){
@@ -57,28 +65,29 @@ public class Apply_transforms : MonoBehaviour
                                                       displacement.z *Time.time);
 
         angle =Mathf.Atan2(displacement.z, displacement.x) * Mathf.Rad2Deg;
+        //en caso de tener dos desplazamientos se hará esta operación para obtener el ángulo de rotación
         
-        Matrix4x4 rotate = HW_Transforms.RotateMat(angle, rotationAXIS);
-        Matrix4x4 composite =  move*rotate;
-        //en unity se lee al revés ya que se habla de operaciones con matrices por lo tanto composite se mueve y luego rota
+        Matrix4x4 rotate = HW_Transforms.RotateMat(angle, rotationAXIS);//matrix de rotación
+        Matrix4x4 composite =  move*rotate;//matriz compuesta en la cual primero se rota el carro y después se mueve en esa dirección
+        
 
-        //matrices para las ruedas 
+        //matrices para las ruedas, una matrix de traslación para cada rueda
         Matrix4x4 W1 = HW_Transforms.TranslationMat(wheel[0].position.x,
-                                                                    wheel[0].position.y,
-                                                                    wheel[0].position.z);
+                                                    wheel[0].position.y,
+                                                    wheel[0].position.z);
 
         Matrix4x4 W2 = HW_Transforms.TranslationMat(wheel[1].position.x,
-                                                                    wheel[1].position.y,
-                                                                    wheel[1].position.z);
+                                                    wheel[1].position.y,
+                                                    wheel[1].position.z);
 
         Matrix4x4 W3 = HW_Transforms.TranslationMat(wheel[2].position.x,
-                                                                    wheel[2].position.y,
-                                                                    wheel[2].position.z);
+                                                    wheel[2].position.y,
+                                                    wheel[2].position.z);
 
         
         Matrix4x4 W4 = HW_Transforms.TranslationMat(wheel[3].position.x,
-                                                                    wheel[3].position.y,
-                                                                    wheel[3].position.z);
+                                                    wheel[3].position.y,
+                                                    wheel[3].position.z);
 
         //matrix de rotacion para las ruedas, con que se cree una se puede aplicar despues para todas 
         Matrix4x4 rotate_wheel1 = HW_Transforms.RotateMat(wheel[0].rotation * Time.time, AXIS.X);
@@ -89,6 +98,7 @@ public class Apply_transforms : MonoBehaviour
         Matrix4x4 wheel_composite2 = composite * W2 * rotate_wheel1;
         Matrix4x4 wheel_composite3 = composite * W3 * rotate_wheel1;
         Matrix4x4 wheel_composite4 =  composite * W4 * rotate_wheel1;
+        //aquí se mueve relativamente al carro, por eso se multiplica por la matriz compuesta del carro
 
 
         //Carro
@@ -137,6 +147,7 @@ public class Apply_transforms : MonoBehaviour
         //Normales para las ruedas y el carro
         mesh.vertices = newVertices;
         mesh.RecalculateNormals();
+        //normales para la lista de ruedas 
         for (int i = 0; i<wheel.Length; i++){
             wheel[i].mesh.vertices = wheel[i].newVertices;
             wheel[i].mesh.RecalculateNormals();
@@ -150,10 +161,12 @@ public class Apply_transforms : MonoBehaviour
 [System.Serializable] 
 public class Wheel{
 
+    //se declara la mesh, los vertices y los newvertices para cada rueda
     public Mesh mesh;
     public Vector3[] vertices;
     public Vector3[] newVertices;
 
+    //se declara la posición y el ángulo de rotación para cada rueda
     [SerializeField] public Vector3 position;
     [SerializeField] public float rotation;
 
